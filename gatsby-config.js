4cd48915b0f6fe0927b0f73dc2ec42cea59a070a
root <root@e2e-100-238.ssdcloudindia.net>
2024-01-12 18:43:03 +0530

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,26 @@ module.exports = {
         // false will enable clarity tracking code on production environment only
         enable_on_dev_env: true
       },
-  }
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      // The property ID; the tracking code won't be generated without it
+      trackingId:process.env.GOOGLE_ANALYTICS_ID,
+      // Defines where to place the tracking script - `true` in the head and `false` in the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      exclude: ["/preview/**", "/do-not-track/me/too/"],
+      // Delays sending pageview hits on route update (in milliseconds)
+      pageTransitionDelay: 0,
+      // Enables Google Optimize using your container Id
+      enableWebVitalsTracking: true,
+    },
+  },
     // 'gatsby-plugin-offline'
   ],
 
