1f5dfde67383b59a5687009801348d40eabdbf6c
root <root@e2e-103-66.ssdcloudindia.net>
2024-01-12 12:13:56 +0530

    changes in blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-git-lastmod`,
-     
+
     // ...
     {
       resolve: `gatsby-plugin-sitemap`,
@@ -37,8 +37,8 @@ module.exports = {
         serialize: ({ path, pageContext }) => {
           return {
             url: path,
-            changefreq: 'weekly', 
-            priority: path === '/' ? 1 : 0.7,            
+            changefreq: 'weekly',
+            priority: path === '/' ? 1 : 0.7,
             lastmod: pageContext?.lastMod,
           }
         },
@@ -46,32 +46,32 @@ module.exports = {
     },
     {
       resolve: "gatsby-source-strapi",
-      
+
       options: {
         apiURL:process.env.GATSBY_API_URL,
         accessToken:process.env.API_KEY,
         collectionTypes: [
           'homepage','solution','technology','about','carrer','blog','newsroom','contact','diagnostic','clinician','healthcare','hospital','radiologist','blog-post','casestudy','casestudypost','footer','privacypolicy','complianceandregulatory','newsroomseo','rxpert','protocall','osteocheck'
         ],
-        
+
         queryLimit: 1000,
       },
     },
-   
-    {
-      resolve: `gatsby-plugin-clarity`,
-      options: {
-        // String value for your clarity project ID
-        clarity_project_id: process.env.CLARITY_KEY,
-        // Boolean value for enabling clarity while developing
-        // true will enable clarity tracking code on both development and production environments
-        // false will enable clarity tracking code on production environment only
-        enable_on_dev_env: true
-      },
-  }
+
+  //   {
+  //     resolve: `gatsby-plugin-clarity`,
+  //     options: {
+  //       // String value for your clarity project ID
+  //       clarity_project_id: process.env.CLARITY_KEY,
+  //       // Boolean value for enabling clarity while developing
+  //       // true will enable clarity tracking code on both development and production environments
+  //       // false will enable clarity tracking code on production environment only
+  //       enable_on_dev_env: true
+  //     },
+  // }
     // 'gatsby-plugin-offline'
   ],
-  
+
 }
 
-// 
+//
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -30,45 +30,41 @@ function Post({ data, location }) {
 
   return (
     <>
-    <Helmet>
+    {/* <Helmet>
       <html lang="en" />
       <title>{content.seo.metaTitle}</title>
       <meta name="description" content={content.seo.metaDescription.data.metaDescription} />
       <link rel="icon" href= {tablogo}
         type="image/x-icon"/>
-      {/* <meta name="author" content={author} /> */}
-    </Helmet>
+    </Helmet> */}
     <div className='bg-black Homepage'>
     <Navbar />
       {/* mobile-responsive */}
       <Navbarmd/>
       <div className='blog-section sm:pt-[7rem] sm:p-[1rem]'>
         <div className='max-w-full flex justify-center ml-auto mr-auto mb-[165px]  sm:mt-0 '>
-          <div className='blog-wrapper w-[1200px] max-w-full flex flex-col justify-center items-center'>
-            <h1 className=' w-full  tracking-[0] mb-[16px] text-[56px] leading-[120%] mt-[6rem] sm:mt-0 sm:text-[30px] text-[#fff]'>
+          <div className='blog-wrapper w-[42%] max-w-full flex flex-col justify-center items-center mt-[11rem] sm:w-full sm:mt-0'>
+            {/* <h1 className=' w-full  tracking-[0] mb-[16px] text-[56px] leading-[120%] mt-[6rem] sm:mt-0 sm:text-[30px] text-[#fff]'>
               {content.Title}
-            </h1>
+            </h1> */}
 
-            <img src={content.Heroimage.localFile.url} className=' w-full max-w-full mt-[40px] mb-[80px] pt-0 pb-0 align-middle'></img>
+            {/* <img src={content.Heroimage.localFile.url} className=' w-full max-w-full mt-[40px] mb-[80px] pt-0 pb-0 align-middle'></img> */}
 
             <div className=' w-[1200px] max-w-full flex flex-col justify-center items-center'>
               <div className='blog_content text-[16px] leading-[150%]' style={{ color: 'rgba(255,255,255,0.8)' }}>
-                <p className='mb-[12px] texxt-[18px] leading-[170%] font-use' style={{ color: 'rgba(255,255,255,0.8)' }}>
+                {/* <p className='mb-[12px] texxt-[18px] leading-[170%] font-use' style={{ color: 'rgba(255,255,255,0.8)' }}>
                 <div className='font-use' dangerouslySetInnerHTML={{ __html: content.Blog_content.data.Blog_content }}></div>
-                  {/* <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{content.Blog_content.data.Blog_content}</ReactMarkdown> */}
-                  {/* {ReactHtmlParser(content.Blog_content.data.Blog_content)} */}
                 </p>
                 <div className=''>
                   <img src={content.content_image.localFile.url} className=' w-full max-w-full mt-[40px] mb-[80px] pt-0 pb-0 align-middle ml-auto mr-auto'></img>
-                </div>
+                </div> */}
                 <div className='flex'>
                   <p className='mb-[12px] text-[18px] leading-[170%] font-use' style={{ color: 'rgba(255,255,255,0.8)' }}>
                   <div className='font-use' dangerouslySetInnerHTML={{ __html: content.content.data.content}}></div>
                     {/* <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{content.content.data.content}</ReactMarkdown> */}
                   </p>
                 </div>
-                  {console.log("slug values from cms",content.slug)}
-                <div className=' flex gap-[8rem] justify-between mt-[20rem] sm:mt-[5rem] sm:flex-col'>
+                {/* <div className=' flex gap-[8rem] justify-between mt-[20rem] sm:mt-[5rem] sm:flex-col'>
                 <a href={content.Recent_post_url}>
                   <div className=' flex flex-col '>
                     <img src={content.Recent_post_image.localFile.url} className=' w-full h-auto'></img>
@@ -80,7 +76,7 @@ function Post({ data, location }) {
                     <img src={content.CTA_image.localFile.url} className=' w-full h-auto'></img>
                     <label className=' mt-[8px] w-fit'>{content.CTA_title}</label>
                   </div></a>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
