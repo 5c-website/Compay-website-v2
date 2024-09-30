641eb88a518da4293e0310a69b8ccaf4b87d8d46
Rokeshkumar-5c <rokeshkumar@5cnetwork.com>
2024-09-30 16:26:09 +0530

    UPDATE: career header text

--- src/components/ReachUs.jsx
+++ src/components/ReachUs.jsx
@@ -124,7 +124,7 @@ function ReachUs() {
         </div>
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Careers@5cnetwork.com
+            Careers @ 5C Network
           </h2>
         </div>
         <form
