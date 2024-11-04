04a6f2b75d156f3286e7f94e5b3ea5aff9f5d78b
Rokeshkumar-5c <rokeshkumar@5cnetwork.com>
2024-11-04 16:08:50 +0530

    FIX: google verification

---
+++ gatsby-ssr.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta
+      key="google-site-verification"
+      name="google-site-verification"
+      content="rseFcJj_imY3-SmUmb4ncLtE6lDXepsoeLE2prsSc8M"
+    />,
+  ]);
+};
