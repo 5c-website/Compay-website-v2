d92cb8345788fdf7b7d37f5fad0884dbcff9910f
Rokeshkumar-5c <rokeshkumar@5cnetwork.com>
2025-08-28 16:04:22 +0530

    UPDATE: added research tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,6 +58,7 @@ const Navbar = ({ topClassName }) => {
       route: "/impact/blogs",
     },
     { label: "Explore 5C", route: "/explore-5C" },
+    { label: "Research", route: "/research" },
     { label: "Contact Us", route: "/contact-us" },
   ];
 
